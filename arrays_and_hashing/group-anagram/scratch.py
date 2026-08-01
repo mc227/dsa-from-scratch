f94f56614c26d752278e4478978cac24d997ed0d
Mark <markcostales.mark@gmail.com>
2026-07-31 23:44:03 -0400

    in between

--- arrays_and_hashing/group-anagram/scratch.py
+++ arrays_and_hashing/group-anagram/scratch.py
@@ -1,22 +1,23 @@
 def groupAnagrams(mylist):
-    main = {}
-    for item in mylist:
-        hashmap = {}
-        for letter in item:
-            if letter in hashmap:
-                hashmap[letter] = hashmap[letter]+1
-            else:
-                hashmap[letter] = 1
-        signature = tuple(sorted(hashmap.items()))
+    hashmap = {}
+    for word in mylist:
+        freq_list = [0] * 26
+        for letter in word:
+            freq_list[ord(letter) - ord('a')]+=1
+    char = 'a'
+    freq_string =[]
+    print(freq_list)
+    for item in freq_list:
+        freq_string.append(char)
+        freq_string.append(item)
+        char = chr(ord('a')+1)
+    if freq_string in hashmap:
+        hashmap[freq_string] = []
+    else:
+        hashmap[freq_string].append()
 
-        if signature in main:
-            main[signature].append(item)
-        else:
-            main[signature] = [item]
-    return list(main.values())
-
-
-print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+# print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(groupAnagrams(["eat"]))
 # [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
 # def groupAnagram(mylist):
 #     main = {}

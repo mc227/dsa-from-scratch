edab34ea2715ed9af086a233a1050bce9687fe17
mc227 <markcostales.mark@gmail.com>
2026-08-05 08:20:34 -0400

    i'm at the last step of the optimized solution for top k frequent elements

--- arrays_and_hashing/0347-top-k-frequent-elements/scratch.py
+++ arrays_and_hashing/0347-top-k-frequent-elements/scratch.py
@@ -2,20 +2,30 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # get the frequency list first
         freq_list = {}
-        for num in nums:
-            if num in freq_list:
-                freq_list[num] +=1
+        list_of_lists = []
+        for item in nums:
+            if item not in freq_list:
+                freq_list[item] = 1
             else:
-                freq_list[num] = 1
-        print(f"freq_list {freq_list}")
-        foo = sorted(freq_list.values())[-k:]
-        foo_list = []
-        for k,v in freq_list.items():
-            if v in foo:
-                foo_list.append(k)
-        return foo_list
-    
+                freq_list[item] += 1
+        for i in range(len(nums)):
+            list_of_lists.append([])
+        for key, value in freq_list.items():
+            list_of_lists[value].append(key)
+        # given the length of nums create a list of list where we 
+        # 0  1   2    3  4 5
+        #   [3] [2]  [1] 
+        #[[0],[3],[2],[1],[],[]]
+        
+        # the index of the list of list represents the number of occurence
+        # and the list within that index contains the item being counted
+        # lastly do len(list) < k loop from the top
+        # don't sort just add the values until you output the topfrequent
+
+        for i in range(len(list_of_lists)):
+            print(i)
 
 # print(sorted(Solution().topKFrequent([1,1,1,2,2,3], 2)))
 print(Solution().topKFrequent([1,1,1,2,2,3], 2))

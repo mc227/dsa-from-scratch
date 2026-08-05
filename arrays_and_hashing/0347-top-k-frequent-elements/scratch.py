e343e3678d428f072d08c23ffd94916bcfc5b8e4
mc227 <markcostales.mark@gmail.com>
2026-08-05 07:56:49 -0400

    brute force solution

--- arrays_and_hashing/0347-top-k-frequent-elements/scratch.py
+++ arrays_and_hashing/0347-top-k-frequent-elements/scratch.py
@@ -2,6 +2,20 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+        freq_list = {}
+        for num in nums:
+            if num in freq_list:
+                freq_list[num] +=1
+            else:
+                freq_list[num] = 1
+        print(f"freq_list {freq_list}")
+        foo = sorted(freq_list.values())[-k:]
+        foo_list = []
+        for k,v in freq_list.items():
+            if v in foo:
+                foo_list.append(k)
+        return foo_list
+    
 
-print(sorted(Solution().topKFrequent([1,1,1,2,2,3], 2)))
+# print(sorted(Solution().topKFrequent([1,1,1,2,2,3], 2)))
+print(Solution().topKFrequent([1,1,1,2,2,3], 2))
--- arrays_and_hashing/0347-top-k-frequent-elements/solutions.py
+++ arrays_and_hashing/0347-top-k-frequent-elements/solutions.py
@@ -8,13 +8,25 @@
 >>> sorted(Solution().topKFrequent([1,2,1,2,1,2,3,1,3,2], 2))
 [1, 2]
 '''
-
 from typing import List
+
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
-
+        freq_list = {}
+        for num in nums:
+            if num in freq_list:
+                freq_list[num] +=1
+            else:
+                freq_list[num] = 1
+        # print(f"freq_list {freq_list}")
+        foo = sorted(freq_list.values())[-k:]
+        foo_list = []
+        for k,v in freq_list.items():
+            if v in foo:
+                foo_list.append(k)
+        return foo_list
+    
 
 # Do not edit any code below this line!
 

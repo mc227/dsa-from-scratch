32f782b175a1f4e8367346810529200879fe0478
mc227 <markcostales.mark@gmail.com>
2026-08-08 00:53:04 -0400

    added brute force solution for 392

--- arrays_and_hashing/0392-is-subsequece/scratch.py
+++ arrays_and_hashing/0392-is-subsequece/scratch.py
@@ -6,13 +6,27 @@ class Solution:
         # two pointers: i walks s, j walks t
         i = 0
         j = 0
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False    
         while i < len(s) and j < len(t):
-            pass  # TODO
+            if s[i] == t[j] and i<=j and i == (len(s)-1):
+                return True
+            elif s[i] == t[j] and i<=j:
+                i+=1
+                j+=1
+                # print(f"inside if {i} {j}")
+            else:
+                j+=1
+                # print(f"inside else {i} {j}")
         return False
 
 
-print(Solution().isSubsequence("abc", "ahbgdc"))
-
+# print(Solution().isSubsequence("abc", "ahbgdc"))
+# print(Solution().isSubsequence("axc", "ahbgdc"))
+print(Solution().isSubsequence("", "ahbgdc"))
+# print(len(""))
 
 '''
 >>> Solution().isSubsequence("abc", "ahbgdc")
--- arrays_and_hashing/0392-is-subsequece/solutions.py
+++ arrays_and_hashing/0392-is-subsequece/solutions.py
@@ -1,31 +1,42 @@
 from typing import List
 
+"""
+>>> Solution().isSubsequence("abc", "ahbgdc")
+True
+
+>>> Solution().isSubsequence("axc", "ahbgdc")
+False
+
+>>> Solution().isSubsequence("", "ahbgdc")
+True
+
+>>> Solution().isSubsequence("abc", "")
+False
+
+>>> Solution().isSubsequence("acb", "ahbgdc")
+False
+"""
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        """
-        >>> Solution().isSubsequence("abc", "ahbgdc")
-        True
-
-        >>> Solution().isSubsequence("axc", "ahbgdc")
-        False
-
-        >>> Solution().isSubsequence("", "ahbgdc")
-        True
-
-        >>> Solution().isSubsequence("abc", "")
-        False
-
-        >>> Solution().isSubsequence("acb", "ahbgdc")
-        False
-        """
         # two pointers: i walks s, j walks t
         i = 0
         j = 0
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False    
         while i < len(s) and j < len(t):
-            pass  # TODO
+            if s[i] == t[j] and i<=j and i == (len(s)-1):
+                return True
+            elif s[i] == t[j] and i<=j:
+                i+=1
+                j+=1
+                # print(f"inside if {i} {j}")
+            else:
+                j+=1
+                # print(f"inside else {i} {j}")
         return False
-
 
 # Do not edit any code below this line!
 

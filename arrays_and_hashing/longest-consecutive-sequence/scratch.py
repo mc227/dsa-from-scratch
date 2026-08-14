bc3154c6d58a7e4e950a7cdbff2841494ac5fc09
mc227 <markcostales.mark@gmail.com>
2026-08-14 11:12:34 -0400

    bank

--- arrays_and_hashing/longest-consecutive-sequence/scratch.py
+++ arrays_and_hashing/longest-consecutive-sequence/scratch.py
@@ -5,6 +5,17 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if nums == []:
             return 0
+        contains_negative_number = False
+        negatives = []
+        for i in nums:
+            if i <= 0:
+                contains_negative_number = True
+                negatives.append(i)
+
+        if contains_negative_number:
+            return negatives            
+
+
         largest_num = max(nums)
         foo = 0
         bins = [0] * (largest_num+1)
@@ -25,10 +36,11 @@ class Solution:
         
 
 
-# print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
-# print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
-# print(Solution().longestConsecutive([1,0,1,2]))
-# print(Solution().longestConsecutive([]))
+print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
+print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(Solution().longestConsecutive([1,0,1,2]))
+print(Solution().longestConsecutive([]))
+print(Solution().longestConsecutive([0,-1]))
 
 '''
 >>> Solution().longestConsecutive([100, 4, 200, 1, 3, 2])

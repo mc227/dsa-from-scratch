2b897f03c81ecdcdc35c0275f5800b054a2e3a70
mc227 <markcostales.mark@gmail.com>
2026-08-14 11:28:03 -0400

    banks

--- arrays_and_hashing/longest-consecutive-sequence/scratch.py
+++ arrays_and_hashing/longest-consecutive-sequence/scratch.py
@@ -3,44 +3,27 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if nums == []:
-            return 0
-        contains_negative_number = False
-        negatives = []
-        for i in nums:
-            if i <= 0:
-                contains_negative_number = True
-                negatives.append(i)
+        longestConsecutive = []
+        consecutive = 1
+        sorted_nums = sorted(nums)
 
-        if contains_negative_number:
-            return negatives            
-
-
-        largest_num = max(nums)
-        foo = 0
-        bins = [0] * (largest_num+1)
-        for num in nums:
-            # print(num)
-            bins[num] = 1
-        mark = []
-        for item in bins:
-            if item:
-                foo+=1
-            else:
-                if foo:
-                    mark.append(foo)
-                    foo = 0
-        if item:
-            mark.append(foo)
-        return max(mark)        
+        for i in range(len(sorted_nums)-1):
+            if sorted_nums[i]+1 == sorted_nums[i+1]:
+                consecutive+=1
+            elif sorted_nums[i]+1 != sorted_nums[i+1]:
+                longestConsecutive.append(consecutive)
+                consecutive = 1
+        print(longestConsecutive)
+        # consider the last item also
+        # return longestConsecutive
         
 
 
 print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
-print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
-print(Solution().longestConsecutive([1,0,1,2]))
-print(Solution().longestConsecutive([]))
-print(Solution().longestConsecutive([0,-1]))
+# print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+# print(Solution().longestConsecutive([1,0,1,2]))
+# print(Solution().longestConsecutive([]))
+# print(Solution().longestConsecutive([0,-1]))
 
 '''
 >>> Solution().longestConsecutive([100, 4, 200, 1, 3, 2])
